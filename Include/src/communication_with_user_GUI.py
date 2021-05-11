ce7ba4d8c1d272575592a023f3987affe99bac6a
Kriss <geraltrivia19@gmail.com>
2021-05-11 12:18:03 +0200

    Two gui information

---
+++ Include/src/communication_with_user_GUI.py
@@ -0,0 +1,7 @@
+def quantity_enter_number_gui():
+    return 'How many number will you enter: '
+
+
+def information_about_quantity_enter_number_gui():
+    return 'You can choice only integer number from 1 to infinity.'
+
